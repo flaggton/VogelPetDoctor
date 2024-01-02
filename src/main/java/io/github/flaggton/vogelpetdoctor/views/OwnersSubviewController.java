76026e907d63e26e24759ae7219ef87642fa4e03
GAMER <markusvogel12@gmail.com>
2024-01-02 23:49:40 +0100

    impl tableView owners

--- src/main/java/io/github/flaggton/vogelpetdoctor/views/OwnersSubviewController.java
+++ src/main/java/io/github/flaggton/vogelpetdoctor/views/OwnersSubviewController.java
@@ -2,20 +2,27 @@ package io.github.flaggton.vogelpetdoctor.views;
 
 import com.wedasoft.simpleJavaFxApplicationBase.hibernateUtil.HibernateQueryUtil;
 import io.github.flaggton.vogelpetdoctor.data.Owner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-
-import java.util.List;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class OwnersSubviewController {
-
     @FXML
-    ListView<Owner> listViewOwners;
+    TableView<Owner> tableViewOwners;
 
     public void init() {
         try {
-            List<Owner> ownerList = HibernateQueryUtil.Finder.findWithBuilder(Owner.class).findAll();
-            listViewOwners.getItems().addAll(ownerList);
+            ObservableList<Owner> owners = FXCollections.observableArrayList(HibernateQueryUtil.Finder.findWithBuilder(Owner.class).findAll());
+
+            addColumn(tableViewOwners, "ID", "id");
+            addColumn(tableViewOwners, "First Name", "firstName");
+            addColumn(tableViewOwners, "Last Name", "lastName");
+            addColumn(tableViewOwners, "E-Mail", "email");
+            addColumn(tableViewOwners, "Date of birth", "dateOfBirth");
+            tableViewOwners.setItems(owners);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -25,5 +32,11 @@ public class OwnersSubviewController {
 //        } catch (HibernateQueryUtilException e) {
 //            throw new RuntimeException(e);
 //        }
+    }
+
+    private void addColumn(TableView<Owner> tableView, String columnTitle, String fieldName) {
+        TableColumn<Owner,String> firstNameCol = new TableColumn<>(columnTitle);
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<>(fieldName));
+        tableView.getColumns().add(firstNameCol);
     }
 }
